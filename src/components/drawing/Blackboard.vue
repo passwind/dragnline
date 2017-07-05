0dd01ff03ccfb07a71a1c4b7b377bec4ec731d49
Zhu Yu <mike.zhuyu@gmail.com>
2017-07-05 18:50:01 +0800

    Add background

---
+++ src/components/drawing/Blackboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <el-row>
+      <h1>Let's draw something</h1>
+    </el-row>
+    <el-row>
+      control
+    </el-row>
+    <el-row>
+      <div class="drawing-space">
+      </div>
+    </el-row>
+  </div>
+</template>
+
+<style>
+.drawing-space {
+  margin: 0 auto;
+  width: 800px;
+  height: 600px;
+  background: #f2f4fa;
+  background-image: radial-gradient(#c6cad1 2px, transparent 0);
+  background-size: 20px 20px;
+}
+</style>
