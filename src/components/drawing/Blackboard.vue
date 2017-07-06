ff65947dd7a2adf694b22c10cdb1d024c092f9bf
Zhu Yu <mike.zhuyu@gmail.com>
2017-07-06 17:50:33 +0800

    Add draggable support

--- src/components/drawing/Blackboard.vue
+++ src/components/drawing/Blackboard.vue
@@ -8,41 +8,26 @@
     </el-row>
     <el-row>
       <div class="drawing-space">
-          <vue-draggable-resizable :w="100" :h="100" v-on:dragging="onDrag" v-on:resizing="onResize" :parent="true">
-            <p>Hello! I'm a flexible component. You can drag me around and you can resize me.<br>
-            X: {{ x }} / Y: {{ y }} - Width: {{ width }} / Height: {{ height }}</p>
-          </vue-draggable-resizable>
+        <node v-for="node in nodes" :node="node" :key="node.id"></node>
       </div>
     </el-row>
   </div>
 </template>
 
 <script>
-import VueDraggableResizable from 'vue-draggable-resizable'
+import Node from './Node'
 
 export default{
-  components: {VueDraggableResizable},
+  components: { Node },
   data: function () {
     return {
-      width: 0,
-      height: 0,
-      x: 0,
-      y: 0
+      nodes: []
     }
   },
   methods: {
     addDiv () {
       console.log('add a div')
-    },
-    onResize: function (x, y, width, height) {
-      this.x = x
-      this.y = y
-      this.width = width
-      this.height = height
-    },
-    onDrag: function (x, y) {
-      this.x = x
-      this.y = y
+      this.nodes.push({id: 'node-' + (this.nodes.length + 1)})
     }
   }
 }
