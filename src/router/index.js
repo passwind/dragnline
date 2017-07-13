1a3282076389351752f277e9f942037e2043f730
Zhu Yu <mike.zhuyu@gmail.com>
2017-07-13 17:24:28 +0800

    Add connection

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Blackboard from '@/components/drawing/Blackboard'
+import Jquery from '@/components/drawing/Jquery'
 
 Vue.use(Router)
 
 export default new Router({
+  linkActiveClass: 'active',
+  mode: 'history', // do not use /#/.
   routes: [
+    {
+      path: '/jquery',
+      name: 'Jquery',
+      component: Jquery
+    },
     {
       path: '/',
       name: 'Blackboard',
